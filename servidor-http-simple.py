f77de791320b08aa7cdc71f5ce39a454e3d11c92
ChristianBermejo <cbermejog7gmail.com>
2017-02-11 12:15:56 +0100

    Aplicacion Web generadora de URLs aleatorias

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# Ejercicio 14.3 Aplicacion Web generadora de URLs aleatorias
+# Christian Bermejo Guerrero
 
 """
 Simple HTTP Server
@@ -8,6 +10,7 @@ TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
 """
 
 import socket
+import random
 
 # Create a TCP objet socket and bind it to a port
 # We bind to 'localhost', therefore only accepts connections from the
@@ -16,7 +19,7 @@ import socket
 # let's use one above 1024
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
+mySocket.bind(('localhost', 1235))
 
 # Queue a maximum of 5 TCP connection requests
 
@@ -26,11 +29,16 @@ mySocket.listen(5)
 #  (in an infinite loop)
 
 while True:
-    print 'Waiting for connections'
+    print ('Waiting for connections')
     (recvSocket, address) = mySocket.accept()
-    print 'HTTP request received:'
-    print recvSocket.recv(1024)
-    recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
-                    "\r\n")
+    print ('HTTP request received:')
+    print (recvSocket.recv(1024))
+    randomURL = random.randint(1,99999999)
+    recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
+                    "<html><body><h1>Hola.</h1></body></html>\r\n","utf-8"))
+    # PYTHON 3
+    # bytes fix--> a bytes-like object is required, not str
+    # utf-8 fix--> string argument without encoding
+    recvSocket.send(bytes("<h1><A HREF=" + str(randomURL) +
+                          ">Dame Otra</h1></A>", "utf-8"))
     recvSocket.close()
